ea13ad17d59d8b4ba63f94e0628612aec0af8115
papiezonator <52249311+papiezonator@users.noreply.github.com>
2024-09-18 21:32:53 +0200

    succesfully did all the tasks

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x-y
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if (array.length == 0){
+    return 0;
+  } 
+  let starter = 0;
+  let result;
+  for (let i = 0; i < array.length; i++){
+    result = starter+=array[i]
+  }
+  return result;
+  
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let starter = array[0];
+  let result;
+  for (let i = 1; i < array.length; i++){
+    result = starter*=array[i]
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+	return Math.pow(x,y)
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	let result = 1
+  let b = x
+  for (let i = 1; i < b; i++){
+    result = (x*=i)
+  }
+  return result
 };
 
 // Do not edit below this line
