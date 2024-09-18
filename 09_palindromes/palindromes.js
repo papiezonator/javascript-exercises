10cee4eb285a6c39b79824430bb5a9f7a87882d4
papiezonator <52249311+papiezonator@users.noreply.github.com>
2024-09-18 21:31:38 +0200

    succesfully did this exercise, the loop skips punctuation marks instead of removing, which was my primary idea

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
+const palindromes = function (sentence) {
+    let lowerCase = sentence.toLowerCase();
+    let palindrome = [];
+    let reversePalindrome = [];
+    for(let i = 0; i < lowerCase.length; i++){
+        if(lowerCase[i] == ' ' || lowerCase[i] == ',' || lowerCase[i] == '.' || lowerCase[i] == '!'){
+        continue;   
+        }
+        palindrome.push(lowerCase[i]);
+        reversePalindrome.push(lowerCase[i]);
+    }
 
+  reversePalindrome.reverse();
+    
+  console.log("regular: ", palindrome);
+  console.log("reversed: ", reversePalindrome);
+  if(reversePalindrome.join('') == palindrome.join('')){
+        return true
+    } else {
+        return false
+    }  
 };
 
-// Do not edit below this line
 module.exports = palindromes;
+
+
