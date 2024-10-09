a03ed501f406ccd673bdcd874f59f486c4d16239
papiezonator <52249311+papiezonator@users.noreply.github.com>
2024-10-09 21:49:03 +0200

    solved the exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let bookFinder = books.map(bookTitle => bookTitle.title);
+    return bookFinder;
 };
 
 // Do not edit below this line
