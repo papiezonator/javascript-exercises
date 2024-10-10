fb6d368849aea6c05ae456d28340fb62f7dd9c78
papiezonator <52249311+papiezonator@users.noreply.github.com>
2024-10-10 19:24:24 +0200

    solved the fibonacci sequence exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(counter) {
+    let sum = 0;
+    let array = [0,1];
+    if(counter < 0){
+        return("OOPS");
+    }
+    for(let i = 0; i < counter; i++){
+        sum=(array[array.length-1])+(array[array.length-2]);
+        array.push(sum);
+    }
+    return array[counter];
 };
 
 // Do not edit below this line
